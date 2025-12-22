6c5b7879c6497937342e2acaafec4399b3f6ef94
spycrab <wialex121@gmail.com>
2025-12-21 23:35:11 -0500

    feat: express app

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,20 @@
-console.log('Express Tutorial')
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("./public"));
+
+app.get("/api/v1/test", (req, res) => {
+  res.status(500).json({
+    message: "It workded!",
+  });
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>page not found</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`server live at: http://localhost:${port}`);
+});
