246b9efbe4507e0e8811fbd807c5ad44e869ac61
spycrab <wialex121@gmail.com>
2026-01-05 20:44:53 -0500

    feat: add crud operations

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,7 +1,23 @@
 const getAllTasks = (req, res) => {
-  res.send("all tasks");
+  res.send("get all tasks");
 };
 
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+const getTask = (req, res) => {
+  res.send({ id: req.params.id });
+};
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+const deleteTask = (req, res) => {
+  res.send("delete taks");
+};
 module.exports = {
   getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 };
