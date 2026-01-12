2a8a1cd782061a28a093356287edbffb4a15afcb
spycrab <wialex121@gmail.com>
2026-01-12 07:19:18 -0500

    feat: error handling

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(express.static("./public"));
@@ -14,6 +15,8 @@ app.use(express.json());
 //routes
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
+
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
