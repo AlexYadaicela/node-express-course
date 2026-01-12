739998bc52097b31b4ce22d50d3a59f9e07ddf8d
spycrab <wialex121@gmail.com>
2026-01-12 06:22:32 -0500

    feat: error handling and input validation

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -7,14 +7,10 @@ const app = express();
 const port = 3000;
 
 // middleware
-
+app.use(express.static("./public"));
 app.use(express.json());
 
 //routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
 
 // app.get("/api/v1/tasks")         - get all the tasks
