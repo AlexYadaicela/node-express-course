4f1b21ee9481787a9745373f96575ebab469346a
spycrab <wialex121@gmail.com>
2026-01-13 22:09:22 -0500

    feat: numeric fields

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,16 +1,16 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
+  const products = await Product.find({ price: { $gt: 30 } })
     .sort("name")
-    .select("name")
+    .select("name price")
     .limit(10)
-    .skipt(5);
+    .skip(5);
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { feature, company, name, sort, fields } = req.query;
+  const { feature, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (feature) {
@@ -25,6 +25,31 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+
+    const regEX = /\b(>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEX,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    const options = ["price", "rating"];
+
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+  console.log(queryObject);
   let result = Product.find(queryObject);
 
   // sort result based on user indication
@@ -41,10 +66,10 @@ const getAllProducts = async (req, res) => {
     result = result.select(sortList);
   }
 
+  // manage pagination
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
-
   result = result.skip(skip).limit(limit);
 
   const products = await result;
